64de0ede921a707953c1ebff2f49dad2308136ed
salimovhusnud4-byte <salimovhusnud4@gmail.com>
2026-09-09 17:45:12 +0500

    Auto-populate DB on Railway startup

--- start.sh
+++ start.sh
@@ -5,6 +5,10 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --noinput
 
+# Server va Kategoriyalarni bazaga avtomatik qo'shish (faqat yo'q bo'lsa qo'shadi)
+python add_anarxiya1.py
+python add_extra_cats.py
+
 # Start the Telegram bot in the background
 python bot.py &
 
